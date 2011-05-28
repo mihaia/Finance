7bf904e3a218e0546de48f4970f2c34f21a2f3e0
Mihai A.S <mihaia@gmail.com>
2011-05-28 12:20:04 +0200

    first file

---
+++ fetch_lcl_data.py
@@ -0,0 +1,150 @@
+import urllib
+import urllib2
+import cookielib
+import sqlite3
+import sys
+from sgmllib import SGMLParser
+
+class LCLDataFetcher:
+	def __init__(self, agence, compte, code):
+		self._agence = agence
+		self._compte = compte
+		self._code = code
+		self._login_url = "https://particuliers.secure.lcl.fr/everest/UWBI/UWBIAccueil?DEST=IDENTIFICATION"
+		self._data_url = "https://particuliers.secure.lcl.fr/outil/UWLM/ListeMouvementsPar/accesListeMouvementsPar?agence=00474&compte=064596W&mode=06"
+		self._cj = cookielib.LWPCookieJar()
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self._cj))
+		urllib2.install_opener(opener)
+
+	def UpdateCookie(self):
+		values = {'agenceId' : self._agence, 'serviceId' : 'CLI', 'compteId': self._compte, 'CodeId':self._code }
+		data = urllib.urlencode(values)
+		req = urllib2.Request(self._login_url, data)
+		urllib2.urlopen(req)
+		# Now the cookie has been populated automatically in the Cookie Jar.
+
+	def GetAccountData(self):
+		req = urllib2.Request(self._data_url)
+		response = urllib2.urlopen(req)
+		return response.read()
+		
+
+class ExpenseLister(SGMLParser):
+	def reset(self):                              
+		SGMLParser.reset(self)
+		self.entries = []
+		self._process_data = False
+		self._item = 0
+		self._in_td = False
+		self._td_count = 0
+		self._expenses = []
+
+	def start_tr(self, attrs):
+		class_attr = [v for k, v in attrs if k=='class']
+		if class_attr and (class_attr[0] == 'tbl1' or class_attr[0] == 'tbl2'):
+			self._process_data = True
+			self._td_count = 0
+	def end_tr(self):                     
+		if self._process_data:
+			self._process_data = False
+
+	def start_td(self, attrs):
+		self._td_count = self._td_count + 1
+		if not self._process_data:
+			return
+		self._in_td = True
+	def end_td(self):
+		if not self._process_data:
+			return
+		self._in_td = False
+	def handle_data(self, text):
+		if self._in_td:
+			if text.strip():
+				txt = text.strip()
+				if self._td_count == 3:
+					txt = "-" + txt
+				self.entries.append(txt)
+	def get_expenses(self):
+		def ParseValue(value):
+			return float(value.replace(',', '.').replace("\xc2\xa0", ''))
+		if not self._expenses:
+			for i in range(0, len(self.entries), 3):
+				expense = [self.entries[i], self.entries[i + 1], ParseValue(self.entries[i + 2])]
+				self._expenses.append(expense)
+		return self._expenses
+			
+
+class SQLWriter:
+	def __init__(self, filename):
+		self._connection = sqlite3.connect(filename)
+		cursor = self._connection.cursor()
+		try:
+			cursor.execute("select * from cheltuieli")
+		except sqlite3.OperationalError:
+			#create database
+			cursor.execute('create table cheltuieli(date date, name text, category text, sum real)')
+		self._connection.commit()
+		cursor.close()
+	def addItem(self, item):
+		cursor = self._connection.cursor()
+		cursor.execute('select * from cheltuieli where date=? and name=? and category=? and sum=?', item)
+		if cursor.fetchone():
+			return False
+		cursor.execute('insert into cheltuieli values (?,?,?,?)', item)
+		self._connection.commit()
+		cursor.close()
+		return True
+			
+def ParseExpense(expense):
+	category = "no_category"
+	name = expense[1]
+	if "FRANPRIX" in name or "MONOPRIX" in name:
+		category = "EPICERIE"
+	elif "RETRAIT" in name:
+		category = "CASH"
+	elif "VIREMENT AMARAND" in name:
+		category = "ECONOMIE"
+	elif "ASSURANCE CAPITAL MULTICOMPTES" in name or "LCL A LA CARTE" in name:
+		category = "LCL"
+	elif "PRLV GROUPAMA" in name or "PRLV EDF" in name or "PRLV Free" in name or "PRLV VIRGIN" in name:
+		category = "FACTURI"
+	elif "AMAZON" in name:
+		category = "BOOKS & STUFF"
+	elif "AIR FRANCE" in name:
+		category = "VOYAGE"
+	elif "CMP IMMOBILIER" in name:
+		category = "CHIRIE"
+	elif "VIR" in name:
+		category = "DATORII"
+	return [name, expense[1], category, expense[2]]
+
+def main():
+	fetcher = LCLDataFetcher('0474', '064596W', '130184')
+	fetcher.UpdateCookie()
+	page = fetcher.GetAccountData()
+	#print page
+	parser = ExpenseLister()
+	parser.feed(page)
+	expenses = parser.get_expenses()
+	sql_writer = SQLWriter('/code/cheltuilei.db')
+	new_expenses = []
+	processed_expenses = []
+	for expense in expenses:
+		if expense in processed_expenses:
+			continue
+		processed_expenses.append(expense)
+		# some expenses, like air france tickets appear in double
+		# only register one expense in this case
+		expense_count = expenses.count(expense)
+		for i in range(0, expense_count - 1):
+			expenses.remove(expense)
+		expense[-1] = expense[-1] * expense_count
+		parsed_expense = ParseExpense(expense)
+		if sql_writer.addItem(parsed_expense):
+			new_expenses.append(parsed_expense)
+	if new_expenses:
+		print '\n'.join(';'.join(str(entry).replace(".",",") for entry in tpl) for tpl in new_expenses)
+	
+	
+if __name__ == '__main__':
+	main()
